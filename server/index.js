e1d8d2954899547000985f01743ba07336bc1897
Amrinder Singh <amrinder1650@gmail.com>
2025-02-04 21:25:30 -0800

    Completed back end of post team

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 var models = require('./models.js');
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 const PORT = process.env.PORT || 8080;
 
@@ -13,6 +15,16 @@ app.get('/', (req, res) => {
 
 app.get('/teams', (req, res) => {
   models.teams.get((err, results) => {
+    if (err) {
+      res.status(500).send(err.message);
+    } else {
+      res.status(200).send(results);
+    }
+  })
+})
+
+app.post('/teams', (req, res) => {
+  models.teams.post(req.body, (err, results) => {
     if (err) {
       res.status(500).send(err.message);
     } else {
--- server/models.js
+++ server/models.js
@@ -11,6 +11,16 @@ module.exports = {
           callback(null, results.rows);
         }
       })
+    },
+    post: function(data, callback) {
+      const query = `insert into teams (team, wins, losses) values ('${data.team}', 0, 0);`;
+      db.query(query, (err, results) => {
+        if (err) {
+          callback(err);
+        } else {
+          callback(null, results);
+        }
+      })
     }
   }
 }
